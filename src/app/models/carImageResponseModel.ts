cb52ff62280dad90fee1dd0cea745a5fe96aa780
byali <cmpunk_ilgin@hotmail.com>
2023-01-21 15:51:20 +0300

    carRental-frontend

---
+++ src/app/models/carImageResponseModel.ts
@@ -0,0 +1,8 @@
+import { CarImage } from "./carImage";
+import { ResponseModel } from "./responseModel";
+
+
+export interface CarImageResponseModel extends ResponseModel{
+    data:CarImage[]
+    
+}
